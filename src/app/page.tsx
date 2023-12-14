f869f41935645c0254a855db90d8fe6bfdfeb9e3
Joel Ljunggren <joelljunggren@hotmail.com>
2023-12-15 00:41:19 +0100

    Titelchange

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
 
   return <>
     <header className="flex justify-between items-center mb-20">
-      <h1 className="text-2xl">Why though?</h1>
+      <h1 className="text-2xl">Welcome to Whyer</h1>
       <Link
       className="border border-slate-300 text-slate-300 px-2 py-1 rounded
       hover:bg-slate-700 focus-within:bg-slate-700 outline-none" 
